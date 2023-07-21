a97498b5bd005464da640676775d87c53fb52535
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-07-21 15:05:22 +0300

    add hw-03-mongodb

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,38 @@
 import express from "express";
-import ctrl from "../../controllers/contacts.js";
+import ctrl from "../../controllers/contacts-controller.js";
+import { validateBody } from "../../decorators/index.js";
+import contactsSchemas from "../../schemas/contacts-schemas.js";
+import { isEmptyBody, isValidId } from "../../middlewares/index.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", ctrl.getAll);
 
-contactsRouter.get("/:id", ctrl.getById);
+contactsRouter.get("/:id", isValidId, ctrl.getById);
 
-contactsRouter.post("/", ctrl.add);
+contactsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(contactsSchemas.contactAddSchema),
+  ctrl.add
+);
 
-contactsRouter.delete("/:id", ctrl.deleteById);
+contactsRouter.delete("/:id", isValidId, ctrl.deleteById);
 
-contactsRouter.put("/:id", ctrl.updateById);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  isEmptyBody,
+  validateBody(contactsSchemas.contactAddSchema),
+  ctrl.updateById
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  isEmptyBody,
+  validateBody(contactsSchemas.contactUpdateFavoriteSchema),
+  ctrl.updateStatusContact
+);
 
 export default contactsRouter;
