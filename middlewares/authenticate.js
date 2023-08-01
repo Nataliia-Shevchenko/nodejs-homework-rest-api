5e2fefd1c4fe42ad1d030983e16a298613a820f5
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-08-01 16:49:16 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -10,7 +10,7 @@ const authenticate = async (req, res, next) => {
 
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     throw HttpError(401);
   }
   try {
